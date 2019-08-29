cba33479ad3868fcfa507f95e60fe0201b84b193
arkaisho <arkaisho@hotmail.com>
2019-08-29 09:34:14 -0300

    drawer

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -3,26 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 import 'screen_login.dart';
 
-
 class HomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
-
+class _MyHomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   AnimationController animationController;
   Animation animation;
 
-  void mylistener(status){
-    if(status == AnimationStatus.completed){
-    animationController.forward();
+  void mylistener(status) {
+    if (status == AnimationStatus.completed) {
+      animationController.forward();
     }
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     animationController = AnimationController(
       vsync: this,
@@ -32,69 +31,85 @@ class _MyHomePageState extends State<HomePage> with SingleTickerProviderStateMix
     animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
       parent: animationController,
       curve: Curves.bounceInOut,
-     ))..addStatusListener(mylistener);
-     animationController.forward();
+    ))
+      ..addStatusListener(mylistener);
+    animationController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: new AppBar(title: new Text("Página inicial"),backgroundColor: Color(0xFF3CB371),),
-      endDrawer: Drawer( 
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[            
-            ListTile(
-              title: Text('Fazer Questionário'),
-              trailing: new Icon(Icons.assignment),
-              onTap: (){
-                //adicionar algo
+      key: _scaffoldKey,
+      bottomNavigationBar: new BottomAppBar(
+        elevation: 0,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                _scaffoldKey.currentState.openEndDrawer();
               },
             ),
-            ListTile(
-              title: Text('Cadastrar paciente'),
-              trailing: new Icon(Icons.person_add),
-              onTap: (){
-                Navigator.push(
-                  context,
-                   MaterialPageRoute(builder: (context) => AnimationTeste()),
-                );
-              },
+          ],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                ListTile(
+                  title: Text('Perfil'),
+                  trailing: Icon(Icons.person_outline),
+                  onTap: () => {},
+                ),
+                ListTile(
+                  title: Text('Home'),
+                  trailing: Icon(Icons.home),
+                  onTap: () => {},
+                ),
+                ListTile(
+                  title: Text('Configurações'),
+                  trailing: Icon(Icons.settings),
+                  onTap: () => {},
+                ),
+                ListTile(
+                  title: Text('Sair'),
+                  trailing: Icon(Icons.exit_to_app),
+                  onTap: () => {},
+                ),
+              ],
             ),
-            ListTile(
-              title: Text('Sair'),
-              trailing: new Icon(Icons.exit_to_app, color: Colors.red,),
-              onTap: (){
-                Navigator.push(
-                  context,
-                   MaterialPageRoute(builder: (context) => ScreenLogin()),
-                );
-              },
-            )
           ],
         ),
       ),
       body: AnimatedBuilder(
         animation: animationController,
-        builder: (context, child) =>
-        Transform(
-          transform: Matrix4.translationValues(animation.value * width, 0.0, 0.0),
-            child: Container(
-              child: new Column(
-                children: <Widget>[
-                  Text('Testando essa animação',
-                    style: TextStyle(
+        builder: (context, child) => Transform(
+          transform:
+              Matrix4.translationValues(animation.value * width, 0.0, 0.0),
+          child: Container(
+            child: new Column(
+              children: <Widget>[
+                Text(
+                  'Testando essa animação',
+                  style: TextStyle(
                       fontSize: 30.0,
                       fontWeight: FontWeight.normal,
-                      color: Colors.grey
-                    ),
-                  ),
-                ],
-              ),
-               height: MediaQuery.of(context).size.height,
-               width: MediaQuery.of(context).size.width,
-            ),          
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          ),
         ),
       ),
     );
